7c9f99fddc445cdf713e71deb90dddade44ed1cc
Liliana Matos <liliana@1stdibs.com>
2019-04-15 14:39:22 -0400

    FI-920 pass willSendResponse to graphqlResponseToAsyncIterable

--- packages/apollo-server-core/src/runHttpQuery.ts
+++ packages/apollo-server-core/src/runHttpQuery.ts
@@ -1,6 +1,9 @@
 import { $$asyncIterator, createAsyncIterator } from 'iterall';
 import { Request, Headers } from 'apollo-server-env';
 import {
+  GraphQLError,
+} from 'graphql';
+import {
   default as GraphQLOptions,
   resolveGraphqlOptions,
 } from './graphqlOptions';
@@ -258,6 +261,7 @@ export async function processHTTPRequest<TContext>(
   let body: string;
   let isDeferred = false;
   let response;
+  let requestContext;
 
   try {
     if (Array.isArray(requestPayload)) {
@@ -269,7 +273,7 @@ export async function processHTTPRequest<TContext>(
       const responses = await Promise.all(
         requests.map(async request => {
           try {
-            const requestContext = buildRequestContext(request);
+            requestContext = buildRequestContext(request);
             return await processGraphQLRequest(options, requestContext);
           } catch (error) {
             // A batch can contain another query that returns data,
@@ -291,7 +295,7 @@ export async function processHTTPRequest<TContext>(
       const request = parseGraphQLRequest(httpRequest.request, requestPayload);
 
       try {
-        const requestContext = buildRequestContext(request);
+        requestContext = buildRequestContext(request);
         response = await processGraphQLRequest(options, requestContext);
         isDeferred = isDeferredGraphQLResponse(response);
         const initialResponse = isDeferred
@@ -352,6 +356,7 @@ export async function processHTTPRequest<TContext>(
       graphqlResponse: undefined,
       graphqlResponses: graphqlResponseToAsyncIterable(
         response as DeferredGraphQLResponse,
+        requestContext
       ),
       responseInit,
     };
@@ -463,8 +468,9 @@ function cloneObject<T extends Object>(object: T): T {
 /**
  * Note: We can use async generators directly when it is supported in node 8/6
  */
-function graphqlResponseToAsyncIterable(
+function graphqlResponseToAsyncIterable<TContext>(
   result: DeferredGraphQLResponse,
+  requestContext: GraphQLRequestContext<TContext>,
 ): AsyncIterable<string> {
   const initialResponse = prettyJSONStringify(result.initialResponse);
   let initialResponseSent = false;
@@ -484,6 +490,24 @@ function graphqlResponseToAsyncIterable(
                   fromGraphQLError(error),
                 );
               }
+
+              if (value) {
+                 const graphqlResponse = result.willSendResponse({
+                   graphqlResponse: {
+                     ...requestContext.response,
+                     errors: (value.errors as GraphQLError[]),
+                     data: (value.data as Record<string, any>),
+                     extensions: value.extensions,
+                   },
+                   context: requestContext.context,
+                 }).graphqlResponse;
+
+                 value = {
+                   ...value,
+                   extensions: graphqlResponse.extensions,
+                 }
+              }
+
               // Call requestDidEnd when the last patch is resolved
               if (done) result.requestDidEnd();
               return { value: prettyJSONStringify(value), done };
